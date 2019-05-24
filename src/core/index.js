e8b52fd0c8c62452a0b58552f61a44e3bcd9e32d
Jesse R Mykolyn <jesse.mykolyn@groupbyinc.com>
2019-05-24 09:52:22 -0400

    Add comments to Core

--- src/core/index.js
+++ src/core/index.js
@@ -3,6 +3,11 @@
     constructor(opts = {}) {
       this.modules = opts.modules || {};
 
+      // For each module provided via `opts`:
+      // - Extract the module-specific event and callback data.
+      // - Register 1x listener for each 'inbound' event.
+      // - Invoke the corresponding callbacks when an 'inbound' event is emitted.
+      // - Dispatch all 'outbound' events using the data return by each callback.
       Object.keys(this.modules).forEach((k) => {
         const module = this.modules[k];
 
