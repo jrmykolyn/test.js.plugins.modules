256c476bb276f0d0159da6a2a8333222322dd51e
Jesse R Mykolyn <jesse.mykolyn@groupbyinc.com>
2019-05-29 14:29:11 -0400

    Add literate style comments to ComponentA class

--- dist/components/component-a/index.js
+++ dist/components/component-a/index.js
@@ -1,17 +1,32 @@
 "use strict";
+// Each component is defined within an immediately invoked
+// function expression. The wrapping function accepts two parameters:
+// - `window`:       A reference to the global browser context.
+// - `COMPONENTS`:   A reference to the components dictionary.
 ((window, COMPONENTS) => {
-    // Define template.
+    // Here we define the template for the component by:
+    // - Creating a new <template> element and capturing it in a constant.
+    // - Setting the inner HTML of the element.
     const template = document.createElement('template');
     template.innerHTML = `
     <button ref="btn">
       <slot name="content">Component A</slot>
     </button>
   `;
-    // Define component class.
-    COMPONENTS.ComponentA = class ComponentA extends HTMLElement {
+    // Here we create the class that will define the state and behaviour
+    // for our new component.
+    class ComponentA extends HTMLElement {
+        // Then we use the `observedAttributes()` method to specify which
+        // HTML attributes a developer may set or update. In this case,
+        // a developer may set or update the 'outbound-event' attribute.
         static get observedAttributes() {
             return ['outbound-event'];
         }
+        // We use our constructor to:
+        // - Define and attach the Shadow DOM to the component instance.
+        // - Insert the components template into the Shadow DOM.
+        // - Capture references to each significant node within the template.
+        // - Bind the component's methods to the correct context.
         constructor() {
             super();
             this.root = this.attachShadow({ mode: 'open' });
@@ -20,20 +35,38 @@
             // Bind.
             this.fetch = this.fetch.bind(this);
         }
-        // Lifecycle.
+        // Here we use the `connectedCallback()` method to register
+        // a new event listener when the component instance is first
+        // injected into the DOM.
+        //
+        // In this case, we're registering a listen against the
+        // <button> element within the component's template. We
+        // access the <button> element via the `refs` property,
+        // which is set at instantiation time.
         connectedCallback() {
             this.refs.btn.addEventListener('click', this.fetch);
         }
+        // When the component instance is removed from the DOM,
+        // we unregister the <button> click handler.
         disconnectedCallback() {
             this.refs.btn.removeEventListener('click', this.fetch);
         }
-        // API.
+        // The `getRefs()` method generates an object that contains
+        // references to each element within the component's template
+        // that includes a `ref` property. This object is used to
+        // register and unregister event listeners, and in other cases
+        // where DOM access is required.
         getRefs() {
             return [...this.root.querySelectorAll('[ref]') || []]
                 .reduce((acc, node) => {
                 return Object.assign({}, acc, { [node.getAttribute('ref')]: node });
             }, {});
         }
+        // The `fetch()` method emits and event that pierces the Shadow DOM
+        // and bubbles up through the document. If the developer provided
+        // a value for the `outbound-event` attribute, that value is used
+        // for the new event's name. If no `outbound-event` attribute is
+        // provided, a default event name is used.
         fetch() {
             const e = new Event(this.getAttribute('outbound-event') || 'NAMESPACE:DEFAULT', {
                 "bubbles": true,
@@ -41,8 +74,14 @@
             });
             this.refs.btn.dispatchEvent(e);
         }
-    };
-    // Register component.
+    }
+    // After we've defined the component class, we expose it as a property of
+    // the components dictionary (`COMPONENTS`), which was injected as a
+    // parameter of the immediately invoked function expression.
+    COMPONENTS.ComponentA = ComponentA;
+    // Finally, if the current browser supports Web Components, we register
+    // the current component using the 'kebab case' version of the class
+    // name.
     if (window.customElements)
         window.customElements.define('component-a', COMPONENTS.ComponentA);
 })(window, (window.__COMPONENTS__ = window.__COMPONENTS__ || {}));
--- src/components/component-a/index.ts
+++ src/components/component-a/index.ts
@@ -1,7 +1,13 @@
 declare var window: Window;
 
+// Each component is defined within an immediately invoked
+// function expression. The wrapping function accepts two parameters:
+// - `window`:       A reference to the global browser context.
+// - `COMPONENTS`:   A reference to the components dictionary.
 ((window, COMPONENTS) => {
-  // Define template.
+  // Here we define the template for the component by:
+  // - Creating a new <template> element and capturing it in a constant.
+  // - Setting the inner HTML of the element.
   const template = document.createElement('template');
   template.innerHTML = `
     <button ref="btn">
@@ -9,15 +15,24 @@ declare var window: Window;
     </button>
   `;
 
-  // Define component class.
-  COMPONENTS.ComponentA = class ComponentA extends HTMLElement {
+  // Here we create the class that will define the state and behaviour
+  // for our new component.
+  class ComponentA extends HTMLElement {
     root: any;
     refs: any;
 
+    // Then we use the `observedAttributes()` method to specify which
+    // HTML attributes a developer may set or update. In this case,
+    // a developer may set or update the 'outbound-event' attribute.
     static get observedAttributes() {
       return ['outbound-event'];
     }
 
+    // We use our constructor to:
+    // - Define and attach the Shadow DOM to the component instance.
+    // - Insert the components template into the Shadow DOM.
+    // - Capture references to each significant node within the template.
+    // - Bind the component's methods to the correct context.
     constructor() {
       super();
       this.root = this.attachShadow({ mode: 'open' });
@@ -28,16 +43,29 @@ declare var window: Window;
       this.fetch = this.fetch.bind(this);
     }
 
-    // Lifecycle.
+    // Here we use the `connectedCallback()` method to register
+    // a new event listener when the component instance is first
+    // injected into the DOM.
+    //
+    // In this case, we're registering a listen against the
+    // <button> element within the component's template. We
+    // access the <button> element via the `refs` property,
+    // which is set at instantiation time.
     connectedCallback() {
       this.refs.btn.addEventListener('click', this.fetch);
     }
 
+    // When the component instance is removed from the DOM,
+    // we unregister the <button> click handler.
     disconnectedCallback() {
       this.refs.btn.removeEventListener('click', this.fetch);
     }
 
-    // API.
+    // The `getRefs()` method generates an object that contains
+    // references to each element within the component's template
+    // that includes a `ref` property. This object is used to
+    // register and unregister event listeners, and in other cases
+    // where DOM access is required.
     getRefs() {
       return [...this.root.querySelectorAll('[ref]') || []]
         .reduce((acc, node) => {
@@ -48,6 +76,11 @@ declare var window: Window;
         }, {});
     }
 
+    // The `fetch()` method emits and event that pierces the Shadow DOM
+    // and bubbles up through the document. If the developer provided
+    // a value for the `outbound-event` attribute, that value is used
+    // for the new event's name. If no `outbound-event` attribute is
+    // provided, a default event name is used.
     fetch() {
       const e = new Event(this.getAttribute('outbound-event') || 'NAMESPACE:DEFAULT', {
         "bubbles": true,
@@ -57,6 +90,13 @@ declare var window: Window;
     }
   }
 
-  // Register component.
+  // After we've defined the component class, we expose it as a property of
+  // the components dictionary (`COMPONENTS`), which was injected as a
+  // parameter of the immediately invoked function expression.
+  COMPONENTS.ComponentA = ComponentA;
+
+  // Finally, if the current browser supports Web Components, we register
+  // the current component using the 'kebab case' version of the class
+  // name.
   if (window.customElements) window.customElements.define('component-a', COMPONENTS.ComponentA);
 })(window, (window.__COMPONENTS__ = window.__COMPONENTS__ || {}));
